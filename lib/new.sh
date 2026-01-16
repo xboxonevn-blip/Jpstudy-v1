143688cdd77328bd9926cbbf010f4dd456753844
Lư Nguyễn Hoài Chung <xboxonevn@gmail.com>
2026-01-16 12:53:30 +0700

    Add platform .gitignore files and update config
    Added .gitignore files for root, Android, iOS, Linux, macOS, and Windows to improve platform-specific file exclusion. Updated project metadata and configuration paths for Windows build. Also added a helper shell script and updated excluded folders in the project module file.

---
+++ lib/new.sh
@@ -0,0 +1,2 @@
+    flutter doctor --android-licenses
+    
--- windows/flutter/ephemeral/generated_config.cmake
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -1,6 +1,6 @@
 # Generated code do not commit.
 file(TO_CMAKE_PATH "C:\\Users\\xboxo\\flutter" FLUTTER_ROOT)
-file(TO_CMAKE_PATH "C:\\Users\\xboxo\\Documents\\GitHub\\JPstudy-app\\flutter_app" PROJECT_DIR)
+file(TO_CMAKE_PATH "C:\\Users\\xboxo\\Documents\\GitHub\\Jpstudy-v1" PROJECT_DIR)
 
 set(FLUTTER_VERSION "0.1.0+1" PARENT_SCOPE)
 set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
@@ -11,14 +11,14 @@ set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
 # Environment variables to pass to tool_backend.sh
 list(APPEND FLUTTER_TOOL_ENVIRONMENT
   "FLUTTER_ROOT=C:\\Users\\xboxo\\flutter"
-  "PROJECT_DIR=C:\\Users\\xboxo\\Documents\\GitHub\\JPstudy-app\\flutter_app"
+  "PROJECT_DIR=C:\\Users\\xboxo\\Documents\\GitHub\\Jpstudy-v1"
   "FLUTTER_ROOT=C:\\Users\\xboxo\\flutter"
-  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\xboxo\\Documents\\GitHub\\JPstudy-app\\flutter_app\\windows\\flutter\\ephemeral"
-  "PROJECT_DIR=C:\\Users\\xboxo\\Documents\\GitHub\\JPstudy-app\\flutter_app"
-  "FLUTTER_TARGET=C:\\Users\\xboxo\\Documents\\GitHub\\JPstudy-app\\flutter_app\\lib\\main.dart"
-  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049M2I2MmVmYzJhMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\xboxo\\Documents\\GitHub\\Jpstudy-v1\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\xboxo\\Documents\\GitHub\\Jpstudy-v1"
+  "FLUTTER_TARGET=C:\\Users\\xboxo\\Documents\\GitHub\\Jpstudy-v1\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzguNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049M2I2MmVmYzJhMw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049NzhmYzMwMTJlNA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC43"
   "DART_OBFUSCATION=false"
   "TRACK_WIDGET_CREATION=true"
   "TREE_SHAKE_ICONS=false"
-  "PACKAGE_CONFIG=C:\\Users\\xboxo\\Documents\\GitHub\\JPstudy-app\\flutter_app\\.dart_tool\\package_config.json"
+  "PACKAGE_CONFIG=C:\\Users\\xboxo\\Documents\\GitHub\\Jpstudy-v1\\.dart_tool\\package_config.json"
 )
